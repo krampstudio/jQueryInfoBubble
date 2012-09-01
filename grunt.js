0cb9897785d5d296f841d5d2efd5ab5e155f60d9
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2012-09-01 23:56:24 +0200

    Add banner to minified file using grunt

--- grunt.js
+++ grunt.js
@@ -1,8 +1,22 @@
 module.exports = function(grunt){
 	grunt.initConfig({
+		pkg: '<json:package.json>',
+  		meta: {
+    		banner: '/**\n'+
+					' * <%= pkg.name %> - v<%= pkg.version %> \n' +
+					' * @author <%=pkg.author.name%> <<%=pkg.author.email%>>\n' +
+					' * @license <%=pkg.licenses[0].url%>\n'+
+      				' */'
+  		},
 		min : {
 			dist : {
 				src: 'src/jquery.infobubble.js',
+				dest: 'jquery.infobubble.min.js'
+			}
+		},
+		concat : {
+			dist : {
+				src : [ '<banner>', 'jquery.infobubble.min.js'],
 				dest: 'jquery.infobubble.min.js'
 			}
 		}
